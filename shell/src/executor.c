3e20ef02ea1454f6e5e3f967be10f565f9a723dc
keshviagrawal <keshviagrawal11@gmail.com>
2025-09-09 18:25:30 +0000

    .

--- shell/src/executor.c
+++ shell/src/executor.c
@@ -1351,6 +1351,345 @@
 
 
 
+// #define _POSIX_C_SOURCE 200809L
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <stdbool.h>
+// #include <sys/wait.h>
+// #include <sys/types.h>
+// #include <fcntl.h>
+// #include <errno.h>
+// #include <signal.h>
+// #include "../include/executor.h"
+
+// extern pid_t foreground_pid;
+
+// static void execute_single_command(char** tokens, bool run_in_background);
+// static pid_t execute_pipe_group(char** tokens, int token_count, bool run_in_background, const char* command_name);
+
+// /* -----------------------
+//    Background job tracking
+//    ----------------------- */
+// typedef struct BackgroundJob {
+//     int job_number;
+//     pid_t pid;
+//     char command_name[256];
+//     char state[20];
+// } BackgroundJob;
+
+// static BackgroundJob background_jobs[256];
+// static int background_job_count = 0;
+// static int next_job_number = 1;
+
+// static void add_background_job(pid_t pid, const char* command_name) {
+//     if (background_job_count >= (int)(sizeof(background_jobs) / sizeof(background_jobs[0]))) {
+//         return;
+//     }
+//     BackgroundJob* job = &background_jobs[background_job_count++];
+//     job->job_number = next_job_number++;
+//     job->pid = pid;
+//     strcpy(job->state, "Running"); 
+//     if (command_name != NULL) {
+//         strncpy(job->command_name, command_name, sizeof(job->command_name) - 1);
+//         job->command_name[sizeof(job->command_name) - 1] = '\0';
+//     } else {
+//         job->command_name[0] = '\0';
+//     }
+
+//     if (is_interactive_mode) {
+//         // This is only for the '&' case
+//         fprintf(stderr, "[%d] %d\n", job->job_number, (int)job->pid);
+//         fflush(stderr);
+//     }
+// }
+
+// static void remove_background_job_index(int index) {
+//     if (index < 0 || index >= background_job_count) {
+//         return;
+//     }
+//     for (int i = index; i < background_job_count - 1; i++) {
+//         background_jobs[i] = background_jobs[i + 1];
+//     }
+//     background_job_count--;
+// }
+
+// void check_background_jobs(void) {
+//     int status;
+//     for (int i = 0; i < background_job_count; ) {
+//         pid_t pid = background_jobs[i].pid;
+//         pid_t result = waitpid(pid, &status, WNOHANG | WUNTRACED | WCONTINUED);
+        
+//         if (result == 0) {
+//             i++;
+//             continue;
+//         } else if (result == -1) {
+//             remove_background_job_index(i);
+//             continue;
+//         } else {
+//             const char* name = background_jobs[i].command_name[0] != '\0' ? background_jobs[i].command_name : "";
+            
+//             if (WIFEXITED(status)) {
+//                 if (is_interactive_mode) {
+//                     fprintf(stderr, "%s with pid %d exited normally\n", name, (int)pid);
+//                     fflush(stderr);
+//                 }
+//                 remove_background_job_index(i);
+//             } else if (WIFSIGNALED(status)) {
+//                 if (is_interactive_mode) {
+//                     fprintf(stderr, "%s with pid %d exited abnormally\n", name, (int)pid);
+//                     fflush(stderr);
+//                 }
+//                 remove_background_job_index(i);
+//             } else if (WIFSTOPPED(status)) {
+//                 strcpy(background_jobs[i].state, "Stopped");
+//                 i++;
+//             } else if (WIFCONTINUED(status)) {
+//                 strcpy(background_jobs[i].state, "Running");
+//                 i++;
+//             }
+//         }
+//     }
+// }
+
+// static int compare_background_jobs(const void* a, const void* b) {
+//     const BackgroundJob* job_a = (const BackgroundJob*)a;
+//     const BackgroundJob* job_b = (const BackgroundJob*)b;
+//     return strcmp(job_a->command_name, job_b->command_name);
+// }
+
+// void list_activities(void) {
+//     check_background_jobs();
+
+//     BackgroundJob active_jobs[256];
+//     int active_jobs_count = 0;
+
+//     for(int i = 0; i < background_job_count; i++) {
+//         active_jobs[active_jobs_count++] = background_jobs[i];
+//     }
+
+//     qsort(active_jobs, active_jobs_count, sizeof(BackgroundJob), compare_background_jobs);
+
+//     for (int i = 0; i < active_jobs_count; i++) {
+//         printf("[%d] : %s - %s\n", active_jobs[i].pid, active_jobs[i].command_name, active_jobs[i].state);
+//     }
+// }
+
+// void ping(pid_t pid, int signal_number) {
+//     int actual_signal = signal_number % 32;
+
+//     if (kill(pid, actual_signal) == -1) {
+//         if (errno == ESRCH) {
+//             fprintf(stderr, "No such process found\n");
+//         } else {
+//             perror("kill failed");
+//         }
+//     } else {
+//         printf("Sent signal %d to process with pid %d\n", signal_number, (int)pid);
+//     }
+// }
+
+// void check_and_kill_all_jobs(void) {
+//     for (int i = 0; i < background_job_count; i++) {
+//         kill(background_jobs[i].pid, SIGKILL);
+//     }
+//     int status;
+//     while (waitpid(-1, &status, 0) > 0);
+// }
+
+// static void execute_single_command(char** tokens, bool run_in_background) {
+//     int saved_stdin = dup(STDIN_FILENO);
+//     int saved_stdout = dup(STDOUT_FILENO);
+
+//     int in_fd = -1;
+//     int out_fd = -1;
+//     char** cmd_args = tokens;
+
+//     for (int i = 0; tokens[i] != NULL; i++) {
+//         if (strcmp(tokens[i], "<") == 0) {
+//             if (tokens[i+1] == NULL) {
+//                 fprintf(stderr, "syntax error near unexpected token `<'\n");
+//                 exit(1);
+//             }
+//             in_fd = open(tokens[i+1], O_RDONLY);
+//             if (in_fd == -1) {
+//                 fprintf(stderr, "No such file or directory!\n");
+//                 exit(1);
+//             }
+//             tokens[i] = NULL;
+//         } else if (strcmp(tokens[i], ">") == 0) {
+//             if (tokens[i+1] == NULL) {
+//                 fprintf(stderr, "syntax error near unexpected token `>'\n");
+//                 exit(1);
+//             }
+//             out_fd = open(tokens[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+//             if (out_fd == -1) {
+//                 perror(tokens[i+1]);
+//                 exit(1);
+//             }
+//             tokens[i] = NULL;
+//         } else if (strcmp(tokens[i], ">>") == 0) {
+//             if (tokens[i+1] == NULL) {
+//                 fprintf(stderr, "syntax error near unexpected token `>>'\n");
+//                 exit(1);
+//             }
+//             out_fd = open(tokens[i+1], O_WRONLY | O_CREAT | O_APPEND, 0666);
+//             if (out_fd == -1) {
+//                 perror(tokens[i+1]);
+//                 exit(1);
+//             }
+//             tokens[i] = NULL;
+//         }
+//     }
+
+//     if (in_fd != -1) {
+//         dup2(in_fd, STDIN_FILENO);
+//         close(in_fd);
+//     } else if (run_in_background) {
+//         int devnull = open("/dev/null", O_RDONLY);
+//         if (devnull != -1) {
+//             dup2(devnull, STDIN_FILENO);
+//             close(devnull);
+//         }
+//     }
+//     if (out_fd != -1) {
+//         dup2(out_fd, STDOUT_FILENO);
+//         close(out_fd);
+//     }
+
+//     execvp(cmd_args[0], cmd_args);
+
+//     fprintf(stderr, "%s: Command not found\n", cmd_args[0]);
+
+//     if (in_fd != -1) {
+//         dup2(saved_stdin, STDIN_FILENO);
+//     }
+//     if (out_fd != -1) {
+//         dup2(saved_stdout, STDOUT_FILENO);
+//     }
+//     close(saved_stdin);
+//     close(saved_stdout);
+
+//     exit(1);
+// }
+
+// static pid_t execute_pipe_group(char** tokens, int token_count, bool run_in_background, const char* command_name) {
+//     if (token_count <= 0) {
+//         return -1;
+//     }
+
+//     int start = 0;
+//     int prev_pipe[2] = {-1, -1};
+//     pid_t pids[token_count];
+//     int pid_count = 0;
+
+//     for (int i = 0; i <= token_count; i++) {
+//         if (i == token_count || strcmp(tokens[i], "|") == 0) {
+//             int cur_pipe[2] = {-1, -1};
+//             bool is_last = (i == token_count);
+//             if (!is_last) {
+//                 if (pipe(cur_pipe) == -1) {
+//                     perror("pipe failed");
+//                     return -1;
+//                 }
+//             }
+
+//             pid_t pid = fork();
+//             if (pid == -1) {
+//                 perror("fork failed");
+//                 return -1;
+//             } else if (pid == 0) {
+//                 if (!run_in_background) {
+//                     setpgid(0, 0);
+//                     tcsetpgrp(STDIN_FILENO, getpgrp());
+//                 }
+
+//                 if (prev_pipe[0] != -1) {
+//                     dup2(prev_pipe[0], STDIN_FILENO);
+//                     close(prev_pipe[0]);
+//                     close(prev_pipe[1]);
+//                 }
+//                 if (!is_last) {
+//                     dup2(cur_pipe[1], STDOUT_FILENO);
+//                     close(cur_pipe[0]);
+//                     close(cur_pipe[1]);
+//                 }
+
+//                 tokens[i] = NULL;
+//                 execute_single_command(&tokens[start], run_in_background);
+//             } else {
+//                 if (!run_in_background) {
+//                     foreground_pid = pid;
+//                 }
+//                 pids[pid_count++] = pid;
+//                 if (prev_pipe[0] != -1) {
+//                     close(prev_pipe[0]);
+//                     close(prev_pipe[1]);
+//                 }
+//                 if (!is_last) {
+//                     prev_pipe[0] = cur_pipe[0];
+//                     prev_pipe[1] = cur_pipe[1];
+//                 }
+//             }
+//             start = i + 1;
+//         }
+//     }
+
+//     if (run_in_background) {
+//         if (pid_count > 0) {
+//             add_background_job(pids[pid_count - 1], command_name);
+//         }
+//         return pid_count > 0 ? pids[pid_count - 1] : -1;
+//     }
+    
+//     int status;
+//     pid_t result = waitpid(pids[pid_count - 1], &status, WUNTRACED);
+//     tcsetpgrp(STDIN_FILENO, getpgrp());
+    
+//     if (result == -1) {
+//         perror("waitpid");
+//     } else if (WIFSTOPPED(status)) {
+//         add_background_job(pids[pid_count - 1], command_name);
+//         // NEW: Print the required output immediately after the process is added.
+//         fprintf(stderr, "\n[%d] Stopped %s\n", background_jobs[background_job_count-1].job_number, background_jobs[background_job_count-1].command_name);
+//     }
+
+//     foreground_pid = -1;
+
+//     return pid_count > 0 ? pids[pid_count - 1] : -1;
+// }
+
+// bool execute_command(char** tokens, int token_count) {
+//     if (token_count <= 0) {
+//         return false;
+//     }
+
+//     int cmd_start = 0;
+//     for (int i = 0; i < token_count; i++) {
+//         if (strcmp(tokens[i], ";") == 0 || strcmp(tokens[i], "&") == 0) {
+//             bool background = (strcmp(tokens[i], "&") == 0);
+//             tokens[i] = NULL;
+//             const char* command_name = (cmd_start < token_count && tokens[cmd_start] != NULL) ? tokens[cmd_start] : "";
+//             if (execute_pipe_group(&tokens[cmd_start], i - cmd_start, background, command_name) == -1) {
+//                 return false;
+//             }
+//             cmd_start = i + 1;
+//         }
+//     }
+
+//     if (cmd_start < token_count) {
+//         const char* command_name = (tokens[cmd_start] != NULL) ? tokens[cmd_start] : "";
+//         if (execute_pipe_group(&tokens[cmd_start], token_count - cmd_start, false, command_name) == -1) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+
+
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
@@ -1369,9 +1708,6 @@ extern pid_t foreground_pid;
 static void execute_single_command(char** tokens, bool run_in_background);
 static pid_t execute_pipe_group(char** tokens, int token_count, bool run_in_background, const char* command_name);
 
-/* -----------------------
-   Background job tracking
-   ----------------------- */
 typedef struct BackgroundJob {
     int job_number;
     pid_t pid;
--- shell/src/main.c
+++ shell/src/main.c
@@ -762,6 +762,257 @@
 // }
 
 
+
+
+// #define _POSIX_C_SOURCE 200809L
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <string.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
+// #include <pwd.h>
+// #include <fcntl.h>
+// #include <signal.h>
+// #include "../include/parser.h"
+// #include "../include/hop.h"
+// #include "../include/reveal.h"
+// #include "../include/log.h"
+// #include "../include/input_redir.h"
+// #include "../include/executor.h"
+
+// bool is_interactive_mode = true;
+// pid_t foreground_pid = -1; // Global variable to track the foreground process PID
+
+// #define MAX_BUFFER_SIZE 4096
+
+// // Global variable to store the shell's home directory.
+// char SHELL_HOME_DIR[MAX_BUFFER_SIZE];
+
+// void display_prompt() {
+//     char hostname[MAX_BUFFER_SIZE];
+//     char cwd[MAX_BUFFER_SIZE];
+//     char display_path[MAX_BUFFER_SIZE];
+
+//     uid_t uid = geteuid();
+//     struct passwd *pw = getpwuid(uid);
+//     if (pw == NULL) {
+//         perror("getpwuid failed");
+//         return;
+//     }
+//     char *username = pw->pw_name;
+    
+//     if (gethostname(hostname, sizeof(hostname)) != 0) {
+//         perror("gethostname failed");
+//         return;
+//     }
+    
+//     if (getcwd(cwd, sizeof(cwd)) == NULL) {
+//         perror("getcwd failed");
+//         return;
+//     }
+
+//     if (strstr(cwd, SHELL_HOME_DIR) == cwd) {
+//         strcpy(display_path, "~");
+//         strcat(display_path, cwd + strlen(SHELL_HOME_DIR));
+//     } else {
+//         strncpy(display_path, cwd, sizeof(display_path));
+//     }
+    
+//     printf("<%s@%s:%s> ", username, hostname, display_path);
+//     fflush(stdout);
+// }
+
+// // Signal handler for Ctrl-C (SIGINT)
+// void handle_sigint(int signo) {
+//     (void)signo; // To suppress the unused parameter warning
+//     if (foreground_pid != -1) {
+//         // Send SIGINT to the foreground process group
+//         kill(-foreground_pid, SIGINT);
+//     }
+// }
+
+// // Signal handler for Ctrl-Z (SIGTSTP)
+// void handle_sigtstp(int signo) {
+//     (void)signo; // To suppress the unused parameter warning
+//     if (foreground_pid != -1) {
+//         // Send SIGTSTP to the foreground process group
+//         kill(-foreground_pid, SIGTSTP);
+//     }
+// }
+
+
+
+
+// int main() {
+//     is_interactive_mode = isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && isatty(STDERR_FILENO);
+    
+//     char *line = NULL;
+//     size_t len = 0;
+    
+//     char* prev_dir = NULL;
+
+//     if (getcwd(SHELL_HOME_DIR, sizeof(SHELL_HOME_DIR)) == NULL) {
+//         perror("getcwd failed");
+//         return 1;
+//     }
+
+//     // Set up signal handlers for interactive mode
+//     if (is_interactive_mode) {
+//         signal(SIGINT, handle_sigint);
+//         signal(SIGTSTP, handle_sigtstp);
+        
+//         // Ignore SIGTTOU and SIGTTIN to allow background processes to run
+//         signal(SIGTTOU, SIG_IGN);
+//         signal(SIGTTIN, SIG_IGN);
+//     } else {
+//         // In non-interactive mode, set signals to default behavior
+//         signal(SIGINT, SIG_DFL);
+//         signal(SIGTSTP, SIG_DFL);
+//     }
+
+//     init_log();
+
+//     while (1) {
+//         display_prompt();
+        
+//         ssize_t rd = getline(&line, &len, stdin);
+        
+//         if (rd == -1) {
+//             // Ctrl-D detected (EOF)
+//             printf("\nlogout\n");
+//             check_and_kill_all_jobs();
+//             exit(0);
+//         }
+
+//         line[strcspn(line, "\n")] = '\0';
+        
+//         // Check for completed background jobs before parsing new input
+//         check_background_jobs();
+
+//         if (strlen(line) == 0 || strspn(line, " \t\n\r") == strlen(line)) {
+//             continue;
+//         }
+
+//         add_to_log(line);
+
+//         if (!parse_input(line)) {
+//             printf("Invalid Syntax!\n");
+//         } else {
+//             char* line_copy = strdup(line);
+//             char* tokens[100];
+//             int token_count = 0;
+
+//             if (line_copy) {
+//                 tokenize_input(line_copy, tokens, &token_count);
+
+//                 if (token_count > 0) {
+//                     if (strcmp(tokens[0], "hop") == 0) {
+//                         hop(&tokens[1], token_count - 1, &prev_dir, SHELL_HOME_DIR); 
+//                     } 
+//                     else if (strcmp(tokens[0], "reveal") == 0) {
+//                         int saved_stdin = dup(STDIN_FILENO);
+//                         int saved_stdout = dup(STDOUT_FILENO);
+//                         int in_fd = -1, out_fd = -1;
+//                         char* filtered_args[100];
+//                         int filtered_count = 0;
+//                         for (int i = 1; i < token_count; i++) {
+//                             if (tokens[i] == NULL) {
+//                                 continue;
+//                             }
+//                             if (strcmp(tokens[i], "<") == 0) {
+//                                 if (i + 1 >= token_count || tokens[i+1] == NULL) {
+//                                     fprintf(stderr, "syntax error near unexpected token `<'\n");
+//                                     goto restore_fds_reveal;
+//                                 }
+//                                 in_fd = open(tokens[i+1], O_RDONLY);
+//                                 if (in_fd == -1) {
+//                                     fprintf(stderr, "No such file or directory!\n");
+//                                     goto restore_fds_reveal;
+//                                 }
+//                                 i++;
+//                             } else if (strcmp(tokens[i], ">") == 0) {
+//                                 if (i + 1 >= token_count || tokens[i+1] == NULL) {
+//                                     fprintf(stderr, "syntax error near unexpected token `>'\n");
+//                                     goto restore_fds_reveal;
+//                                 }
+//                                 out_fd = open(tokens[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+//                                 if (out_fd == -1) {
+//                                     perror(tokens[i+1]);
+//                                     goto restore_fds_reveal;
+//                                 }
+//                                 i++;
+//                             } else if (strcmp(tokens[i], ">>") == 0) {
+//                                 if (i + 1 >= token_count || tokens[i+1] == NULL) {
+//                                     fprintf(stderr, "syntax error near unexpected token `>>'\n");
+//                                     goto restore_fds_reveal;
+//                                 }
+//                                 out_fd = open(tokens[i+1], O_WRONLY | O_CREAT | O_APPEND, 0666);
+//                                 if (out_fd == -1) {
+//                                     perror(tokens[i+1]);
+//                                     goto restore_fds_reveal;
+//                                 }
+//                                 i++;
+//                             } else {
+//                                 filtered_args[filtered_count++] = tokens[i];
+//                             }
+//                         }
+//                         if (in_fd != -1) {
+//                             dup2(in_fd, STDIN_FILENO);
+//                             close(in_fd);
+//                             in_fd = -1;
+//                         }
+//                         if (out_fd != -1) {
+//                             dup2(out_fd, STDOUT_FILENO);
+//                             close(out_fd);
+//                             out_fd = -1;
+//                         }
+//                         reveal(filtered_args, filtered_count, &prev_dir, SHELL_HOME_DIR);
+//                     restore_fds_reveal:
+//                         if (saved_stdin != -1) {
+//                             dup2(saved_stdin, STDIN_FILENO);
+//                             close(saved_stdin);
+//                         }
+//                         if (saved_stdout != -1) {
+//                             dup2(saved_stdout, STDOUT_FILENO);
+//                             close(saved_stdout);
+//                         }
+//                     }
+//                     else if (strcmp(tokens[0], "log") == 0) {
+//                         handle_log_command(&tokens[1], token_count - 1, &prev_dir, SHELL_HOME_DIR);
+//                     }
+//                     else if (strcmp(tokens[0], "activities") == 0) {
+//                         list_activities();
+//                     }
+//                     else if (strcmp(tokens[0], "ping") == 0) {
+//                         if (token_count != 3) {
+//                             fprintf(stderr, "Syntax: ping <pid> <signal_number>\n");
+//                         } else {
+//                             pid_t pid = (pid_t)strtol(tokens[1], NULL, 10);
+//                             int signal_num = (int)strtol(tokens[2], NULL, 10);
+//                             ping(pid, signal_num);
+//                         }
+//                     }
+//                     else {
+//                         execute_command(tokens, token_count);
+//                     }
+//                 }
+
+//                 for (int i = 0; i < token_count; i++) {
+//                     free(tokens[i]);
+//                 }
+//                 free(line_copy);
+//             }
+//         }
+//     }
+    
+//     free(line);
+//     if (prev_dir) {
+//         free(prev_dir);
+//     }
+//     return 0;
+// }
+
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
@@ -772,6 +1023,7 @@
 #include <pwd.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <errno.h> // Include errno.h
 #include "../include/parser.h"
 #include "../include/hop.h"
 #include "../include/reveal.h"
@@ -780,11 +1032,10 @@
 #include "../include/executor.h"
 
 bool is_interactive_mode = true;
-pid_t foreground_pid = -1; // Global variable to track the foreground process PID
+pid_t foreground_pid = -1; 
 
 #define MAX_BUFFER_SIZE 4096
 
-// Global variable to store the shell's home directory.
 char SHELL_HOME_DIR[MAX_BUFFER_SIZE];
 
 void display_prompt() {
@@ -821,26 +1072,19 @@ void display_prompt() {
     fflush(stdout);
 }
 
-// Signal handler for Ctrl-C (SIGINT)
 void handle_sigint(int signo) {
-    (void)signo; // To suppress the unused parameter warning
+    (void)signo; 
     if (foreground_pid != -1) {
-        // Send SIGINT to the foreground process group
         kill(-foreground_pid, SIGINT);
     }
 }
 
-// Signal handler for Ctrl-Z (SIGTSTP)
 void handle_sigtstp(int signo) {
-    (void)signo; // To suppress the unused parameter warning
+    (void)signo;
     if (foreground_pid != -1) {
-        // Send SIGTSTP to the foreground process group
         kill(-foreground_pid, SIGTSTP);
     }
 }
-
-
-
 
 int main() {
     is_interactive_mode = isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && isatty(STDERR_FILENO);
@@ -855,20 +1099,16 @@ int main() {
         return 1;
     }
 
-    // Set up signal handlers for interactive mode
     if (is_interactive_mode) {
         signal(SIGINT, handle_sigint);
         signal(SIGTSTP, handle_sigtstp);
-        
-        // Ignore SIGTTOU and SIGTTIN to allow background processes to run
         signal(SIGTTOU, SIG_IGN);
         signal(SIGTTIN, SIG_IGN);
     } else {
-        // In non-interactive mode, set signals to default behavior
         signal(SIGINT, SIG_DFL);
         signal(SIGTSTP, SIG_DFL);
     }
-
+    
     init_log();
 
     while (1) {
@@ -877,15 +1117,19 @@ int main() {
         ssize_t rd = getline(&line, &len, stdin);
         
         if (rd == -1) {
-            // Ctrl-D detected (EOF)
-            printf("\nlogout\n");
-            check_and_kill_all_jobs();
-            exit(0);
+            if (errno == EINTR) {
+                // Interrupted by a signal, continue the loop
+                printf("\n");
+                continue;
+            } else {
+                printf("\nlogout\n");
+                check_and_kill_all_jobs();
+                exit(0);
+            }
         }
 
         line[strcspn(line, "\n")] = '\0';
         
-        // Check for completed background jobs before parsing new input
         check_background_jobs();
 
         if (strlen(line) == 0 || strspn(line, " \t\n\r") == strlen(line)) {
